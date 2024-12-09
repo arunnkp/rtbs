1a10a9692bdbf58b3ee68f7068d3337691111ecb
arun <arun@gmail.com>
2024-12-09 13:05:15 +0530

    starting commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './components/Home'
+import Login from './components/Login'
+
+const App = () => {
+  return (
+    <div>
+      Hello
+      <Home/>
+      <Login/>
+    </div>
+  )
+}
+
+export default App
